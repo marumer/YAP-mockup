7ab95a58d491b3a99b13dbdc3af793a6320ebc62
marumer <marumer@cisco.com>
2023-09-05 13:53:27 +1000

    adding CORS

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const projectRouter = require("./routers/projectRouter");
 
-// const CORS_OPTION = {
-//    credentials: true,
-//    origin: process.env.MODE === MODE.dev ? ["http://localhost:3000", "http://127.0.0.1:3000"] : process.env.CORS_ORIGIN,
-//    optionsSuccessStatus: 200,
-// };
+// Load ENV
+if (process.argv.slice(2)[0] === "dev") {
+   require("dotenv").config({ path: "./.env.dev" });
+} else {
+   require("dotenv").config();
+}
+
+const CORS_OPTION = {
+   credentials: true,
+   origin: process.env.MODE === "dev" ? ["http://localhost:3000", "http://127.0.0.1:3000"] : process.env.CORS_ORIGIN,
+   optionsSuccessStatus: 200,
+};
 
 const app = express();
 
 app.use(express.json({ limit: "1mb" }));
-// app.use(cors(CORS_OPTION));
+app.use(cors(CORS_OPTION));
 
 app.use("/api/project", projectRouter);
 
@@ -19,9 +27,9 @@ app.route("*").all((req, res) => {
    res.status(404).json({ message: "invalid endpoint." });
 });
 
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
-// Starting this project with basic in memeory collections
+// Starting this project with basic in memory collections
 global.projects = [];
 
 app.listen(port, () => {
